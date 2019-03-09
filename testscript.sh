e6b3abd480e8193376aaf3083820301d1d71268e
karthikeyareddy <karthikeyareddy.1992@gmail.com>
2019-03-09 14:49:49 +0000

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,5 +3,5 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo ' this for testing the jenkins automatic build'
-echo 'This is for Fifth time testing the automatic build'
+echo 'This is six time testing the automatic build'
 
